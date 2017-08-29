122ab3b1ca2620aa65cf27d677d635773d2a01b6
wvictores <wvictores@gmail.com>
2017-08-29 15:20:18 -0500

    new commits

---
+++ AthenasSpider/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AthenasSpider.Models;
+
+namespace AthenasSpider.Controllers
+{
+    public class ProductController : Controller
+    {
+        //GET: Product Index
+        public ActionResult Index()
+        {
+            var products = new ProductModel[] {
+                new ProductModel{ ItemId = 1, Item = "Dog Collar", Price = 12 },
+                new ProductModel{ ItemId = 3, Item = "Dog Leash", Price = 20 },
+                new ProductModel{ ItemId = 2, Item = "Dog Hat", Price = 24 }
+            };
+            
+            return View(products);
+        }
+    }
+}
---
+++ AthenasSpider/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace AthenasSpider.Models
+{
+    public class ProductModel
+    {
+        [Display(Name = "ItemId")]
+        public int ItemId { get; set; }
+
+        [Required]
+        [Display(Name = "Item")]
+        public string Item { get; set; }
+
+        [Required]
+        [Display(Name = "Price")]
+        public double Price { get; set; }
+    }
+}
